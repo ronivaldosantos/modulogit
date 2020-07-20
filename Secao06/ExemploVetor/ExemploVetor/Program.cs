fc1825c1b65ffeb2c87e34654699ed7f8418ec79
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-07-19 21:18:28 -0300

    Exemplo vetor

---
+++ Secao06/ExemploVetor/ExemploVetor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExemploVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);                
+            }
+
+            double soma = 0;
+            for (int i = 0; i < n; i++) {
+                soma += vect[i];            
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+
+    }
+}
