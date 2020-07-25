970030c467fc79029b64a616adbab61d593207c6
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-07-25 19:52:58 -0300

    Exercicio de matrizes

---
+++ Secao06/Matrizes/ExercicioMatriz/ExercicioMatriz/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os valores M e N da matriz separados por espaço: ");
+            string[] tamanho = Console.ReadLine().Split(' ');
+
+            int m = int.Parse(tamanho[0]);
+            int n = int.Parse(tamanho[1]);
+
+            int[,] mat = new int[m, n];
+
+            Console.WriteLine("Informe os valore para a matriz: ");
+
+            // Preenche as os valores na matriz.
+            for (int i = 0; i < m; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.Write("Informe um número pertencente a matriz: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (numero == mat[i, j] )
+                    {
+                        //Linha/coluna onde está o número informado.
+                        Console.Write("Position: " + i + "," + j);
+                        Console.WriteLine();
+
+                        //Primeiro a esquerda do número
+                        if (j > 0)                        {
+                            Console.Write("Left: " + mat[i, j - 1]);
+                            Console.WriteLine();
+                        }
+
+                        //Primeiro acima do número
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+
+                        //Primeiro a direita do número
+                        if (j < n -1)
+                        {
+                            Console.Write("Right: " + mat[i, j + 1]);
+                            Console.WriteLine();
+                        }
+                      
+                        //Primeiro abaixo do número
+                        if (i < m -1) 
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1 ,j] );                        
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Secao06/Matrizes/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //Declaração de matriz tamanho n X n.
+            int[,] mat = new int[n, n];
+
+            // Percorre as linas da matriz
+            for (int i = 0; i < n; i++)
+            {
+                //Fazendo a leitura da linha digitada
+                string[] values = Console.ReadLine().Split(' ');
+
+                //Percorre as colunas da matriz
+                for (int j = 0; j < n; j++)
+                {
+                    //Insere valores na Matriz
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int negative = 0;
+            //Percorrer a matriz para imprimir os números negativos
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //Quantidade números negativos na matriz
+                    if (mat[i, j] < 0)
+                    {
+                        negative++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Negative numbers = " + negative);
+        }
+    }
+}
